57f9b0caf72f0fb841bb9695e569b0e60863008f
stjuliod09 <124937364+stjuliod09@users.noreply.github.com>
2025-02-05 11:23:41 -0500

    task completed

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -7,10 +7,6 @@ def test_homework():
     """Test the homework."""
 
     assert os.path.exists(
-        "files/data/demanda-comercial-dias.csv"
-    ), "files/data/demanda-comercial-dias.csv file is missing"
-
-    assert os.path.exists(
         "files/plots/demanda-comercial-patrones-ejemplo.png"
     ), "files/plots/demanda-comercial-patrones-ejemplo.png file is missing"
 
